e527b7e643b49674d9b7e3bc852624988368cb19
Sonja Askim <sonja.askim@gmail.com>
2017-12-30 00:27:32 +0100

    Person modal dialog validation

--- Poengrenn.Web/src/app/competition/competition-details/person-modal/person-modal.component.ts
+++ Poengrenn.Web/src/app/competition/competition-details/person-modal/person-modal.component.ts
@@ -3,13 +3,14 @@ import { NgbActiveModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 import { GENDERS } from 'app/_shared/constants/constants';
 
 import { ApiService } from 'app/_services/api.service';
+import { AuthService } from 'app/_services/auth.service';
 import { KonkurranseDeltaker, Konkurranse, Person } from 'app/_models/models';
 
 @Component({
   selector: 'app-create-person-modal-modal',
   templateUrl: './person-modal.component.html',
   styleUrls: ['./person-modal.component.scss'],
-  providers: [ApiService]
+  providers: []
 })
 export class PersonModalComponent implements OnInit {
 
@@ -20,10 +21,12 @@ export class PersonModalComponent implements OnInit {
 
   genders = GENDERS.slice(0, GENDERS.length); 
   duplicates: Person[];
+  errorMessage: string;
 
   constructor(
     public _activeModal: NgbActiveModal,
-    private _apiService: ApiService) { }
+    private _apiService: ApiService,
+    private _authService: AuthService) { }
 
   ngOnInit() {
     this.personModel = new Person();
@@ -31,6 +34,41 @@ export class PersonModalComponent implements OnInit {
       this.personModel = this.editPerson;
     console.log(this.genders);
     this.genders.splice(this.genders.findIndex(g => g.id.toLowerCase() == "mix"), 1);
+  }
+
+  private isAdmin() {
+    return this._authService.isAdmin();
+  }
+
+  hasDuplicates(): boolean {
+    if (this.isAdmin())
+      return false;
+
+    return (this.duplicates && 
+            this.duplicates.findIndex(d => d.fodselsar == this.personModel.fodselsar && d.kjonn == this.personModel.kjonn) > -1);
+  }
+  hasErrors(): boolean {
+    this.errorMessage = "";
+    if (this.isAdmin())
+      return false;
+    
+    if (this.personModel.fodselsar && (this.personModel.fodselsar < 1900 || this.personModel.fodselsar > new Date().getFullYear()))
+    {
+      this.errorMessage += "Vennligst skriv inn et gyldig fødselsår, 4 siffer.";
+      return true;
+    }
+      
+    if (!this.personModel.epost && !this.personModel.telefon) {
+      this.errorMessage += "E-post og/eller telefon må fylles ut.";
+      return true;
+    }
+
+    if (this.hasDuplicates()) {
+      this.errorMessage += `Det er funnet liknende person(er) med samme fødselsår. <br/>
+                            Send en e-post til <a href='mailto:sonja.askim@gmail.com'>sonja.askim@gmail.com</a> hvis du ønsker å opprette ny person med tilsvarende navn.`;
+      return true;
+    }
+    return false;
   }
 
   saveCompetitionParticipant() {
@@ -59,9 +97,7 @@ export class PersonModalComponent implements OnInit {
   }
 
   findDuplicates():void {
-    console.log(this.personModel);
     if (this.personModel.fornavn && this.personModel.etternavn) {
-      console.log(this.personModel);
       this.duplicates = this.existingPersons
                             .filter(e => e.fornavn.toLowerCase().startsWith(this.personModel.fornavn.toLowerCase()) &&
                                          e.etternavn.toLowerCase().startsWith(this.personModel.etternavn.toLowerCase()));
